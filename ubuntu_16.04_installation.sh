8b4fd60811c3784c08165f72b0208c9279d67451
Brian Chen <brianchen.chen@mail.utoronto.ca>
2021-04-02 00:05:57 -0400

    add ubunut 16.04  sort of setup files

---
+++ ubuntu_16.04_installation.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+sudo apt install stow zsh direnv
+sudo snap install nvim --classic
+
+git clone https://github.com/ihasdapie/dotfiles
+cd dotfiles
+stow nvim # will need to remove treesitter and sniprun
+stow zsh
+
+# get antibody
+curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+
+# get fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+
+# get nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# will need to install a recent-er version of node for coc.nvim
+
+
+# get nnn, fd
+wget https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb
+wget https://github.com/jarun/nnn/releases/download/v3.6/nnn_3.6-1_ubuntu16.04.amd64.deb
+
+sudo dpkg -i fd_8.2.1_amd64.deb
+sudo dpkg -i nnn_3.6-1_ubuntu16.04.amd64.deb
+
+
+
+
+
+
+
+
+
+
+
+
