de30ff2fbcafcbaabe5c60f04f9c64ac64258aff
Brian Chen <ihasdapi@gmail.com>
2021-06-09 01:21:37 -0700

    Add emacs

--- nvim/.config/nvim/lua/galaxyline_config.lua
+++ nvim/.config/nvim/lua/galaxyline_config.lua
@@ -149,6 +149,7 @@ gls.left[2] = {
       local vim_mode = vim.fn.mode()
       vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color[vim_mode])
       return alias[vim_mode] .. '   '
+      -- return alias[vim_mode] .. '   '
     end,
     highlight = {colors.red,colors.line_bg,'bold'},
   },
---
+++ nvim/.config/nvim/lua/nvim-bufferline_config.lua
@@ -0,0 +1,6 @@
+require('bufferline').setup{
+    options = {
+        tab_size=12,
+        numbers="both",
+    }
+}
